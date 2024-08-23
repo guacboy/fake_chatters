14c9aa9a19e35805175a056aaada4a7bac176635
Dylan <rattledew17@gmail.com>
2024-08-22 21:22:21 -0700

    added a 'jerma' mode
    added a toggle for general messages (ON by default)
    
    added a randomizer for generating messages

--- src/client.py
+++ src/client.py
@@ -10,17 +10,31 @@ client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
-general_prompt = "You are a casual Twitch/YouTube viewer watching your favorite streamer. You are curious about the streamer's life and want to ask a question in their chat box. \
-    1. Please keep your messages to a maximum of 15 words or less - the less words you use in your messages, the better. \
+general_prompt = "You are a casual Twitch/YouTube viewer watching your favorite streamer. You are curious about the streamer's life and want to ask a question in their chat box. The main goal is to send messages that the streamer can read and interact with. Keep in mind that not all messages have to be questions, you can also send a basic comment regarding the topics. \
+    1. Please keep your messages to a maximum of 20 words or less - the less words you use in your messages, the better. \
     2. Avoid asking multiple questions in one message and try to limit your topic to about one or two - you are having a casual conversation, not an interview. \
-    3. Formulate your messages with internet slang, not capitalizing your letters at the beginning of sentences, imperfect punctuations, etc; however, please keep your messages eligble for the streamer to read. Below are multiple examples of how your message should be structured: \
-        a. 'that was very poggers' \
-        b. 'how's the stream going?' \
-        c. 'damn that's crazy lol, i never heard of that' \
-    4. Do not follow up from previous messages, each message should act as if you are another person; however, avoid using the same topic multiple times - the more unique the topic is from previous topics, the better the message."
+    3. Formulate your messages with internet slang, not capitalizing your letters at the beginning of sentences, imperfect punctuations, emphasis on specific words, excessive use of question/exclamation marks, etc; however, please keep your messages eligble for the streamer to read. Below are multiple examples of how your message can be structured: \
+        a. that was very poggers \
+        b. how's the stream going??? \
+        c. damn that's CRAZY lol, i never heard of that \
+    4. Avoid using quotations in your messages. \
+    5. Do not follow up from previous messages, each message should act as if you are another person; however, avoid using the same topic multiple times - the more unique the topic is from previous topics, the better the message."
+    
+jerma_chat_examples = "Below are comments pulled from Jerma985's streams that you can refer, but not limit to, when creating your messages: \
+    1. SMARTIES ARE GREAT UP YOUR URETHRA \
+    2. if you had a tiny clone of yourself, would you torture it? \
+    3. mustache hair \
+    4. jerma please i've been edging for 3 and a HALF hours \
+    5. penis stream?? \
+    6. This is the poop wine room \
+    7. *JERMA PLEASE READ THIS IS IMPORTANT* Imagine if Jerma's real name was like Harold or something wouldn't that be fucked up \
+    8. ride sucks LULWTRUE \
+    9. Hey Jerma, you're short omegalul \
+    10. HOW ARE YOUR BALLS \
+NOTE: Please do not refer to the streamer as 'Jerma'"
 
 class Client():
-    def openai():
+    def general_message():
         chat_completion = client.chat.completions.create(
         messages=[
             {
@@ -31,6 +45,25 @@ class Client():
                     3. curious question about their lifestyle \
                     4. casual, funny remarks \
                     5. a random comment about recent news"
+            }
+        ],
+        model="gpt-3.5-turbo",
+        )
+        
+        return chat_completion
+    
+    def jerma_message():
+        chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "assistant",
+                "content": f"{general_prompt} You also have a very unique personality. Your messages can appear unhinge and make the streamer question your sanity. The topics may include, but not limited to: \
+                    1. asking a question that feels 'out of pocket' \
+                    2. a very obscure and weird remark about the things you do \
+                    3. poking fun at the streamer with jokes like, but not limited to: \
+                        a. 'your mom' jokes \
+                        b. 'your short' joke \
+                {jerma_chat_examples}"
             }
         ],
         model="gpt-3.5-turbo",
--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from client import Client
 from tkinter import *
+import random
 
 # starting menu - where you can customize what type of messages to expect
 root = Tk()
 root.geometry("480x440")
 
+is_general_button_on = True
 is_jerma_button_on = False
 
 off_image = PhotoImage(file="C:\\Users\\rattl\\Desktop\\Projects\\Python\\fake_chatters\\assets\\off-button.png")
@@ -32,15 +34,30 @@ def toggle_button(button,
     else:
         button.config(image=on_image)
 
+# General Mode
+def general_button_function(general_button) -> bool:
+    global is_general_button_on
+    
+    print("Toggling general mode.")
+    toggle_button(general_button,
+                  is_general_button_on)
+    is_general_button_on = not is_general_button_on
+    
+# creates the toggle button for general mode
+general_button = create_button(on_image)
+general_button.config(command=lambda: general_button_function(general_button))
+general_button.pack()
+
 # Jerma Mode
 def jerma_button_function(jerma_button) -> bool:
     global is_jerma_button_on
     
+    print("Toggling 'Jerma' mode.")
     toggle_button(jerma_button,
                   is_jerma_button_on)
     is_jerma_button_on = not is_jerma_button_on
     
-# creates the toggle button for "Jerma Mode"
+# creates the toggle button for "Jerma" mode
 jerma_button = create_button(off_image)
 jerma_button.config(command=lambda: jerma_button_function(jerma_button))
 jerma_button.pack()
@@ -58,12 +75,27 @@ start_button.pack()
 
 # opens a new window where the chat messages will be displayed
 def display_chat(chat_window):
-    message = Client.openai().choices[0].message.content
+    trait_list = list()
     
-    chat_message = create_label(chat_window)
-    chat_message.config(text=message)
-    chat_message.pack()
-    root.after(60000, display_chat, chat_window)
+    if chat_window.winfo_exists():
+        if is_general_button_on:
+            trait_list.append("general")
+        if is_jerma_button_on:
+            trait_list.append("jerma")
+        
+        random_trait = random.choice(trait_list)
+        
+        if random_trait == "general":
+            print("Generating a general message.")
+            message = Client.general_message().choices[0].message.content
+        if random_trait == "jerma":
+            print("Generating a 'Jerma' message.")
+            message = Client.jerma_message().choices[0].message.content
+            
+        chat_message = create_label(chat_window)
+        chat_message.config(text=message)
+        chat_message.pack()
+        root.after(60000, display_chat, chat_window)
 
 if __name__ == "__main__":
     root.mainloop()
